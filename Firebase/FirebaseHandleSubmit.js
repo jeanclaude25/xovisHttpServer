a86b04d125ff5988b7fc62308d2bd1e618f827df
jeanclaude25 <jeanclaude.stephane@hotmail.fr>
2023-11-17 16:39:42 +0100

    corerction code

--- Firebase/FirebaseHandleSubmit.js
+++ Firebase/FirebaseHandleSubmit.js
@@ -1,20 +1,6 @@
-const { getFirestore, addDoc, collection } = require("@firebase/firestore");
-//const { firestore } = require("./FirebaseSetup.js");
+const { addDoc, collection } = require("@firebase/firestore");
+const firestore = require("./FirebaseSetup");
 
-const { initializeApp } = require('firebase/app');
-
-const firebaseConfig = {
-    apiKey: process.env.APP_apiKey,
-    authDomain: process.env.APP_authDomain,
-    projectId: process.env.APP_projectId,
-    storageBucket: process.env.APP_storageBucket,
-    messagingSenderId: process.env.APP_messagingSenderId,
-    appId: process.env.APP_appId
-};
-
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-//module.exports = firestore;
 
 
 const handleSubmit = async (db_collection, datas_to_send) => {
